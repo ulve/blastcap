e87f1b32d051cd3df7ac18bbe48088d5a9d4e100
Olov Johansson <johansson.olov@gmail.com>
2018-08-31 12:56:48 +0200

    First d3 thing working

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import data from "./data";
+import "./index.css";
+
+ReactDOM.render(
+  <App
+    width={window.screen.availWidth}
+    height={window.screen.availHeight}
+    graph={data}
+  />,
+  document.getElementById("root") as HTMLElement
+);
